cabf3649e526a703d7cbd998afff0600c46e643e
Leonardo de Albuquerque Gouveia <leogouv@gmail.com>
2019-04-22 07:48:57 -0300

    Ajusta css Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,23 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
-    <nav role="navigation" aria-label="main navigation">
-      <div>
+    <nav role="navigation" aria-label="main navigation" className="navbar">
+      <div className="container">
         <div>
-          <div>
-            <NavLink to="/" exact activeClassName="is-active">
-              Home
-            </NavLink>
-          </div>
-          <div>
-            <div>
-              <div>
-                <NavLink to="/new" exact>
-                  Add Post
-                </NavLink>
-              </div>
-            </div>
-          </div>
+          <NavLink to="/" exact activeClassName="is-active">
+            Home
+          </NavLink>
+        </div>
+        <div className="end">
+          <NavLink to="/new" exact>
+            Add Post
+          </NavLink>
         </div>
       </div>
     </nav>
